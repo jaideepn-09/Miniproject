d3130fa732994d012e9ffe5fe0083ba520de43aa
Jaideep N <jaideepn3590@duck.com>
2024-03-04 00:08:34 +0530

    second

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 def establish_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="Admiraljai_69",
-        database="miniproject"
+        user="",
+        password="",
+        database=""
     )
